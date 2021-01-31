a9d5812d14d75d5e8ff4fefeef58d2d72c326bc2
red-naxel-afk <rualex2018@yandex.ru>
2021-02-01 01:09:10 +0300

    Пификшены уровни

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if key[pygame.K_t]:
                 print(level.doors)
+                print(get_cell((hero.pos_x, hero.pos_y)))
 
             if event.type == pygame.QUIT:
                 terminate()
